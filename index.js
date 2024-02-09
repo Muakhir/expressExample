3676ca283b88d96cea32c3b4fe475ed71f543a50
Muakhir <almuakhirjones@gmail.com>
2024-02-09 16:13:17 +0200

    .

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("welcome to express js")
+import express from 'express'
+
+// create an express app
+const app = express()
+const port = +process.env.PORT || 4000
+// router
+app.get('/', (req,res)=>{
+    // res.status(200).json({mgs:"You're Home!"})
+    // res.json({mgs:"You're Home!"})
+    res.json({status:res.statusCode, mgs:"You're Home!"})
+})
+app.get('/about', (req, res)=>{
+    res.json({status:res.statusCode, mgs:"About Page"})
+})
+app.all('*', (req, res)=>{
+    res.json({status:404, mgs:"404 PAGE"})
+})
+
+app.listen(port)
