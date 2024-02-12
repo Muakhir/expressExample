42e958e8fbd1a2f7424b340f7e82ffb43615ff09
Muakhir <almuakhirjones@gmail.com>
2024-02-12 14:58:09 +0200

    .

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 import express from 'express'
-
+import path from 'path'
 // create an express app
 const app = express()
+const router = express.Router()
 const port = +process.env.PORT || 4000
+app.use(router, express.static('./static'))
 // router
-app.get('/', (req,res)=>{
+router.get('/', display, (req,res)=>{
     // res.status(200).json({mgs:"You're Home!"})
-    // res.json({mgs:"You're Home!"})
-    res.json({status:res.statusCode, mgs:"You're Home!"})
-})
-app.get('/about', (req, res)=>{
-    res.json({status:res.statusCode, mgs:"About Page"})
-})
-app.all('*', (req, res)=>{
-    res.json({status:404, mgs:"404 PAGE"})
+    // res.json({mgs:"You're Home!"}
+    res.status(200).sendFile(path.resolve('./static/html/index.html'))
 })
 
+
+// middleware
+
+function display(req, res, next) {
+    console.log('hello there ')
+     next()
+}
+
+
+
+router.get('/about', (req, res)=>{
+    res.json({status:res.statusCode, mgs:"About Page"})
+})
+
+
 app.listen(port)
+
